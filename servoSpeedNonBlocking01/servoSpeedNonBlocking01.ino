441a94ed38271857dfadccf75f35192810ab5f5e
Byron Lahey <Byron.Lahey@asu.edu>
2026-03-03 14:52:00 -0700

    Create initial variables and start movement logic.

--- servoSpeedNonBlocking01/servoSpeedNonBlocking01.ino
+++ servoSpeedNonBlocking01/servoSpeedNonBlocking01.ino
@@ -2,14 +2,32 @@
 #include <elapsedMillis.h>
 
 Servo cameraServo;  // create servo object to control a servo
+const int cameraServoPin = 7;
+
+elapsedMillis cameraMovementTimer;
+int cameraMovementInterval = 50;
+
+int cameraPosition = 90;
+int cameraMaxPosition = 170;
+int cameraMinPosition = 10;
+int cameraDirection = 1; // clockwise = 1; counterclockwise = -1
 
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  cameraServo.attach(myservoPin);  // attaches the servo on pin 9 to the servo object
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+}
+
+void sweepCamera() {
+  if(cameraMovementTimer >= cameraMovementInterval) {
+    cameraMovementTimer = 0; // reset the timer
+
+  }
 
 }
